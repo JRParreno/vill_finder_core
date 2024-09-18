5db189fc809ceb27d1f8de7b738dce65838d2662
JRParreno <jhonrhayparreno22@gmail.com>
2024-09-19 00:04:00 +0800

    remove admin creation

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py 
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python ./manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]];
+# then
+#   python ./manage.py createsuperuser --no-input
+# fi
