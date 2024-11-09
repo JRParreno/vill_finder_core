cf0df3011453a9b12848dbb355a4de8b853f38b9
Jhon Rhay Parreño <jhonrhayparreno@192.168.1.23>
2024-11-09 17:13:39 +0800

    set media root path

--- vill_finder_core/settings.py
+++ vill_finder_core/settings.py
@@ -197,7 +197,7 @@ if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/media/'
 MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
